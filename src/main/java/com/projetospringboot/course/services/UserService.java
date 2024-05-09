0da4fd8ed28147604f252522d8c2be58a278729a
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2024-05-09 17:09:36 -0300

    Exception handling - delete

--- src/main/java/com/projetospringboot/course/services/UserService.java
+++ src/main/java/com/projetospringboot/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projetospringboot.course.entities.User;
 import com.projetospringboot.course.repositories.UserRepository;
+import com.projetospringboot.course.services.exceptions.DatabaseException;
 import com.projetospringboot.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,7 +37,14 @@ public class UserService {
 	
 	//para deletar um usuario do banco de dados
 	public void delete(Long id) { //passa o id do usuario e deleta do banco o usuario q tiver esse id
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	//para atualizar um dado usuario
